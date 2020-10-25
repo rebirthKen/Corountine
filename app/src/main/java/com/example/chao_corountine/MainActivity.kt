2dc9e4ab572df4b6bd8a6ecd8d181004586849a9
chao shi <shichao@hello.com>
2020-10-25 14:24:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chao_corountine/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.chao_corountine
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button: View
+    lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+
+        }
+    }
+}
